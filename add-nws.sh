f4f1883670efbccf4472834addffd339dc0ac7a6
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-01-09 16:22:32 -0600

    Update add-nws.sh

--- add-nws.sh
+++ add-nws.sh
@@ -17,8 +17,8 @@ mkdir .github 2>&1
 mkdir .github/workflows 2>&1
 touch .github/workflows/nws-deploy.yaml 2>&1
 
-wget -O .github/workflows/nws-deploy.yaml https://raw.githubusercontent.com/nickorlow/nws-ghactions-templates/main/$1.yaml
-sed -i 's/{{_main_branchname_}}/$branchname/' .github/workflows/nws-deploy.yaml
+wget -O .github/workflows/nws-deploy.yaml https://raw.githubusercontent.com/nickorlow/nws-ghactions-templates/main/$1.yaml 2>&1
+sed -i "s/{{_main_branchname_}}/$branchname/" .github/workflows/nws-deploy.yaml
 git add .github/workflows/nws-deploy.yaml 2>&1
 git commit -am "Added NWS deployment script" 2>&1
 
