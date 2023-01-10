144af56b70026b895ef9611dd6dc1ea9b2ee7338
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-01-10 09:49:44 -0600

    Update add-nws.sh

--- add-nws.sh
+++ add-nws.sh
@@ -2,7 +2,20 @@ orig_dir=`pwd`
 cur_dir=`mktemp -d`
 
 cd $cur_dir
+echo "
+  _  _  __      __ ___   
+ | \| | \ \    / // __|  
+ | .` |  \ \/\/ / \__ \  
+ |_|\_|   \_/\_/  |___/  
+_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| 
+\"\`-0-0-\'\"\`-0-0-'\"\`-0-0-\' 
+Deployment Helper
+---
 
+
+"
+
+echo "cloning github.com:$2/$3..."
 git clone --quiet git@github.com:$2/$3.git . > /dev/null 2>&1
 
 if [ -f ".github/workflows/nws-deploy.yaml" ]
@@ -10,11 +23,12 @@ then
 echo "NWS Deployment scripts already exist in this repo!"
 echo "Continuing with this script will overwrite those scripts"
 echo "---"
+echo
 fi
 
 echo "Using deployment stragegy $1"
 read -p "Enter the name of your main branch [main]: " branchname  <&1
-
+echo
 if [ -z "$branchname" ]
 then
 branchname="main"
@@ -32,9 +46,9 @@ git add .github/workflows/nws-deploy.yaml > /dev/null 2>&1
 git commit -am "Added NWS deployment script" > /dev/null 2>&1
 
 if git push > /dev/null 2>&1; then
-echo "Welcome to NWS!"
+echo "Pushed deployment script to git repo"
 else
-echo "Pushing git repo failed! (Is there a merge conflict?)"
+echo "Pushing git repo failed! (Do you have access to it?/Is there a merge conflict?)"
 fi
 
 else
@@ -46,7 +60,7 @@ rm -rf $cur_dir
 
 i=0
 x=0
-echo "Waiting for build to finish (this may take a while!)..."
+echo -n "Waiting for build to finish (this may take a while!)"
 while [[ `curl -s -N https://ghcr.io/token\?scope\="repository:$2/$3:pull"` == *"error"* ]]; 
 do
   if [[ "$i" -eq 3 ]]
